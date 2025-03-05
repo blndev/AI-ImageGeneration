a0827736d8bd7dd81533b7d11a1c84044e8e7cd6
blndev <blndev@users.noreply.github.com>
2025-03-05 14:31:19 +0100

    additional startup params

--- main.py
+++ main.py
@@ -3,4 +3,9 @@ from app import GradioUI, setup_logging
 setup_logging()
 if __name__ == "__main__":
     ui = GradioUI()
-    ui.launch(share=False)
+    ui.launch(
+        share=False, 
+        server_name="0.0.0.0",
+        show_api=False,
+        enable_monitoring=False
+    )
