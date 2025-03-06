de84e7420bfa63581c74c7863c2a2420493562a6
blndev <blndev@users.noreply.github.com>
2025-03-06 22:23:43 +0100

    dynamic output dir

--- app/gradioui.py
+++ app/gradioui.py
@@ -20,10 +20,11 @@ class GradioUI():
     def __init__(self):
         self.interface = None
         self.generator = FluxGenerator()
+        self.output_directory = os.getenv("OUTPUT_DIRECTORY", None)
 
     def create_interface(self):
         # Define the generate function that will be called when the button is clicked
-        def uiaction_generate_images(prompt, aspect_ratio, request: gr.Request):
+        def uiaction_generate_images(prompt, aspect_ratio, neg_prompt, image_count, request: gr.Request):
             try:
                 logger.info(f"generating image with prompt: {prompt} and aspect ratio: {aspect_ratio}")
 
@@ -40,47 +41,70 @@ class GradioUI():
                 elif "portrait" in aspect_ratio.lower():  # == "▤ Portrait (2:3)"
                     width, height = 832, 1248
 
+                steps = int(os.getenv("GENERATION_STEPS", 30 if self.generator.is_flux_dev() or self.generator.SDXL else 4))
+                guidance = float(os.getenv("GENERATION_GUIDANCE",
+                                 7.5 if self.generator.is_flux_dev() or self.generator.SDXL else 0))
+
                 generation_details = FluxParameters(
                     prompt=prompt,
-                    # negative_prompt="unrealistic, ugly", # TODO: in ui
-                    # TODO: from config
-                    num_inference_steps=int(os.getenv("GENERATION_STEPS", 30 if self.generator.is_flux_dev() else 4)),
-                    guidance_scale=float(os.getenv("GENERATION_GUIDANCE", 7.5 if self.generator.is_flux_dev() else 0)),
-                    num_images_per_prompt=2,
+                    negative_prompt=neg_prompt,
+                    num_inference_steps=steps,
+                    guidance_scale=guidance,
+                    num_images_per_prompt=image_count,
                     width=width,
                     height=height
                 )
 
                 images = self.generator.generate_images(params=generation_details)
-                logger.info(f"received {len(images)} image(s) from generator")
-                for image in images:
-                    # TODO: path via env
-                    save_image_with_timestamp(image=image, folder_path="./output", ignore_errors=True)
+                logger.debug(f"received {len(images)} image(s) from generator")
+                if self.output_directory is not None:
+                    logger.debug(f"saving images to {self.output_directory}")
+                    for image in images:
+                        outdir = os.path.join(self.output_directory, datetime.now().strftime("%Y-%m-%d"))
+                        save_image_with_timestamp(image=image, folder_path=outdir, ignore_errors=True)
                 return images
             except Exception as e:
                 logger.error(f"image generation failed: {e}")
                 gr.Warning(f"Error while generating the image: {e}")
 
         # Create the interface components
-        with gr.Blocks() as self.interface:
+        with gr.Blocks(title="Image Generator") as self.interface:
             with gr.Row():
-                # Text prompt input
-                prompt = gr.Textbox(
-                    label="Enter your prompt",
-                    placeholder="Describe the image you want to generate...",
-                    value="",
-                    scale=4,
-                    lines=4
-                    # max_lines=4
-                )
+                with gr.Column():
+                    # Text prompt input
+                    prompt = gr.Textbox(
+                        label="Enter your prompt",
+                        placeholder="Describe the image you want to generate...",
+                        value="",
+                        scale=4,
+                        lines=4
+                    )
 
-                # Aspect ratio selection
-                aspect_ratio = gr.Radio(
-                    choices=["□ Square (1:1)", "▤ Landscape (16:9)", "▯ Portrait (2:3)"],
-                    value="□ Square (1:1)",
-                    label="Aspect Ratio",
-                    scale=1
-                )
+                    # Text prompt input
+                    neg_prompt = gr.Textbox(
+                        label="Negative prompt",
+                        placeholder="Describe what you don't want...",
+                        value="",
+                        scale=4,
+                        lines=4
+                    )
+
+                with gr.Column():
+                    # Aspect ratio selection
+                    aspect_ratio = gr.Radio(
+                        choices=["□ Square (1:1)", "▤ Landscape (16:9)", "▯ Portrait (2:3)"],
+                        value="□ Square (1:1)",
+                        label="Aspect Ratio",
+                        scale=1
+                    )
+                    image_count = gr.Slider(
+                        label="Image Count",
+                        minimum=1,
+                        maximum=4,
+                        value=2,
+                        step=1,
+                        scale=1
+                    )
 
                 # Generate button that's interactive only when prompt has text
                 generate_btn = gr.Button(
@@ -92,11 +116,26 @@ class GradioUI():
                     # Examples
                     gr.Examples(
                         examples=[
-                            ["A majestic mountain landscape at sunset with snow-capped peaks", "▤ Landscape (16:9)"],
-                            ["A professional portrait of a business person in a modern office", "▯ Portrait (2:3)"],
-                            ["A top-down view of a colorful mandala pattern", "□ Square (1:1)"]
+                            [
+                                "A majestic mountain landscape at sunset with snow-capped peaks",
+                                "painting",
+                                "▤ Landscape (16:9)",
+                                1
+                            ],
+                            [
+                                "A professional portrait of a business person in a modern office",
+                                "ugly face",
+                                "▯ Portrait (2:3)",
+                                1
+                            ],
+                            [
+                                "A top-down view of a colorful mandala pattern",
+                                "realistic",
+                                "□ Square (1:1)",
+                                1
+                            ]
                         ],
-                        inputs=[prompt, aspect_ratio],
+                        inputs=[prompt, neg_prompt, aspect_ratio, image_count],
                         label="Click an example to load it"
                     )
             with gr.Row():
@@ -121,7 +160,7 @@ class GradioUI():
             # Connect the generate button to the generate function
             generate_btn.click(
                 fn=uiaction_generate_images,
-                inputs=[prompt, aspect_ratio],
+                inputs=[prompt, aspect_ratio, neg_prompt, image_count],
                 outputs=[gallery]
             )
 
