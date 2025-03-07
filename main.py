e2071be64876e3a08d52c828032d3a831dc63457
blndev <1238227+blndev@users.noreply.github.com>
2025-03-07 13:27:02 +0100

    force override existing variables

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from distutils.util import strtobool
 
 setup_logging()
 if __name__ == "__main__":
-    load_dotenv()
+    load_dotenv(override=True)
     ui = GradioUI()
     ui.launch(
         share=bool(strtobool(os.getenv("GRADIO_SHARED", "False"))),
