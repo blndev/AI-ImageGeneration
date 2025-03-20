e4ffc19fd7222f234f6d2f23f9d60df8febfdb89
blndev <blndev@users.noreply.github.com>
2025-03-20 18:20:49 +0100

    startup infos about cuda

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import json
+import logging
 import os
 from dotenv import load_dotenv
 from app import GradioUI, setup_logging
@@ -7,6 +8,41 @@ from app.generators import ModelConfig
 from distutils.util import strtobool
 load_dotenv(override=True)
 setup_logging()
+logger = logging.getLogger("app")
+
+import torch
+
+# Get the current GPU memory usage
+def print_gpu_memory_usage():
+    if torch.cuda.is_available():
+        # Get the GPU ID
+        gpu_id = torch.cuda.current_device()
+        
+        # Get the total and allocated memory
+        total_memory = torch.cuda.get_device_properties(gpu_id).total_memory
+        allocated_memory = torch.cuda.memory_allocated(gpu_id)
+        cached_memory = torch.cuda.memory_reserved(gpu_id)
+        
+        logger.info(f"Total GPU memory: {total_memory / 1024**3:.2f} GB")
+        logger.info(f"Allocated GPU memory: {allocated_memory / 1024**3:.2f} GB")
+        logger.info(f"Cached GPU memory: {cached_memory / 1024**3:.2f} GB")
+    else:
+        pass
+
+def get_gpu_info():
+    # Überprüfen, ob CUDA verfügbar ist
+    if torch.cuda.is_available():
+        # Anzahl der verfügbaren GPUs
+        num_gpus = torch.cuda.device_count()
+        logger.info(f"Available GPUs: {num_gpus}")
+        
+        # Namen der GPUs abrufen
+        for i in range(num_gpus):
+            gpu_name = torch.cuda.get_device_name(i)
+            print(f"GPU {i}: {gpu_name}")
+    else:
+        logger.warning("No CUDA GPUs available")
+
 if __name__ == "__main__":
 
     mc = None
@@ -19,7 +55,10 @@ if __name__ == "__main__":
             except Exception as e:
                 print(f"Startup failed while reading model config from '{mc_path}'")
                 raise 
-    
+
+    get_gpu_info()
+    print_gpu_memory_usage()
+
     ui = GradioUI(modelconfigs=mc)
     ui.launch(
         share=bool(strtobool(os.getenv("GRADIO_SHARED", "False"))),
