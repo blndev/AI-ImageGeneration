8407e8e870cf81e83f9464c865e6667797ea2d06
blndev <1238227+blndev@users.noreply.github.com>
2025-03-24 22:21:44 +0100

    log output

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import json
 import logging
 import os
 from dotenv import load_dotenv
-from app import GradioUI, setup_logging
+from app import AppConfig, GradioUI, setup_logging
 from app.generators import ModelConfig
 
 from distutils.util import strtobool
@@ -12,22 +12,22 @@ logger = logging.getLogger("app")
 
 import torch
 
-# Get the current GPU memory usage
-def print_gpu_memory_usage():
-    if torch.cuda.is_available():
-        # Get the GPU ID
-        gpu_id = torch.cuda.current_device()
+# # Get the current GPU memory usage
+# def print_gpu_memory_usage():
+#     if torch.cuda.is_available():
+#         # Get the GPU ID
+#         gpu_id = torch.cuda.current_device()
         
-        # Get the total and allocated memory
-        total_memory = torch.cuda.get_device_properties(gpu_id).total_memory
-        allocated_memory = torch.cuda.memory_allocated(gpu_id)
-        cached_memory = torch.cuda.memory_reserved(gpu_id)
+#         # Get the total and allocated memory
+#         total_memory = torch.cuda.get_device_properties(gpu_id).total_memory
+#         allocated_memory = torch.cuda.memory_allocated(gpu_id)
+#         cached_memory = torch.cuda.memory_reserved(gpu_id)
         
-        logger.info(f"Total GPU memory: {total_memory / 1024**3:.2f} GB")
-        logger.info(f"Allocated GPU memory: {allocated_memory / 1024**3:.2f} GB")
-        logger.info(f"Cached GPU memory: {cached_memory / 1024**3:.2f} GB")
-    else:
-        pass
+#         logger.info(f"Total GPU memory: {total_memory / 1024**3:.2f} GB")
+#         logger.debug(f"Allocated GPU memory: {allocated_memory / 1024**3:.2f} GB")
+#         logger.debug(f"Cached GPU memory: {cached_memory / 1024**3:.2f} GB")
+#     else:
+#         pass
 
 def get_gpu_info():
     # Überprüfen, ob CUDA verfügbar ist
@@ -37,27 +37,37 @@ def get_gpu_info():
         logger.info(f"Available GPUs: {num_gpus}")
         
         # Namen der GPUs abrufen
-        for i in range(num_gpus):
-            gpu_name = torch.cuda.get_device_name(i)
-            print(f"GPU {i}: {gpu_name}")
+        for gpu_id in range(num_gpus):
+            gpu_name = torch.cuda.get_device_name(gpu_id)
+            total_memory = torch.cuda.get_device_properties(gpu_id).total_memory
+            allocated_memory = torch.cuda.memory_allocated(gpu_id)
+            cached_memory = torch.cuda.memory_reserved(gpu_id)
+            logger.info(f"using GPU {gpu_id}: {gpu_name} / {total_memory}GB (Available {total_memory - allocated_memory}GB)")
     else:
         logger.warning("No CUDA GPUs available")
 
 if __name__ == "__main__":
 
+    config = AppConfig()
     mc = None
-    mc_path = os.getenv("MODELCONFIG", "./modelconfig.json")
+    mc_path = config.modelconfig_json
+    logger.info(f"Initialize modelconfigs from {mc_path}")
     if os.path.exists(mc_path):
         with open(mc_path, "r") as f:
             try:
                 j = f.read()
                 mc = ModelConfig.create_config_list_from_json(j)
+                for m in mc:
+                    logger.debug(f"Available model: '{m.model}'->'{m.parent}' from '{m.path}'")
+                
             except Exception as e:
-                print(f"Startup failed while reading model config from '{mc_path}'")
-                raise 
+                logger.error(f"Startup failed while reading model config from '{mc_path}': {e}")
+                exit(1)
+    else:
+        logger.error(f"File {mc_path} does not exist. Modelconfigs could not be loaded. Exit 1")
+        exit(1)
 
     get_gpu_info()
-    print_gpu_memory_usage()
 
     ui = GradioUI(modelconfigs=mc)
     ui.launch(
